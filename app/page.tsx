a818d9ed1c27775d34076047e2388bfad7c7e17c
bewu <bewuwy@gmail.com>
2024-05-17 20:54:14 +0200

    moved from /rooms to /

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
-import { buttonVariants } from "@/components/ui/button"
-import Link from "next/link";
+"use client";
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function Home() {
+
+  let [city, setCity] = useState("");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>ROOM.nl archive data</h1>
-
-      {/* button with link to /rooms */}
-      <Link className={buttonVariants({ variant: "default" })} href="/rooms">See the data</Link>
+        <h1>ROOM.nl archive data</h1>
+    
+        <form className="flex flex-col gap-4 pt-4" onSubmit={
+          (event) => {
+            event.preventDefault();
+            window.location.href = `/${city}`;
+          }
+        }>
+          <Input
+              placeholder="City..."
+              onChange={(event) => setCity(event.target.value)}
+              className="max-w-sm"
+          />
+          <Button type="submit" onClick={() => {
+            window.location.href = `/${city}`;
+          }}>See data for the city</Button>
+        </form>
     </main>
   );
 }
