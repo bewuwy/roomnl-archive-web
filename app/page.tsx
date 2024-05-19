1f1a11aa8e44d142d858405c19d1e9d2a88661f7
bewuwy <61749397+bewuwy@users.noreply.github.com>
2024-05-19 14:56:01 +0000

    changed input on index to a combobox

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input";
 import { DateRangePicker } from "@/components/ui/datepicker";
+import { Combobox } from "@/components/ui/combobox";
 
 import { useState } from "react";
 
 function redirect(city: string) {
+  if (!city || city === "") {
+    return;
+  }
+
   window.location.href = `/${city}?${new URLSearchParams(window.location.search).toString()}`;
 }
 
 export default function Home() {
 
   let [city, setCity] = useState("");
+
+  let cities = [
+    { label: "Amsterdam", value: "amsterdam" },
+    { label: "Rotterdam", value: "rotterdam" },
+    { label: "Utrecht", value: "utrecht" },
+    { label: "Den Haag", value: "den-haag" },
+    { label: "Eindhoven", value: "eindhoven" },
+  ];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
@@ -24,10 +36,10 @@ export default function Home() {
             redirect(city);
           }
         }>
-          <Input
-              placeholder="City..."
-              onChange={(event) => setCity(event.target.value)}
-              className="max-w-sm"
+
+          <Combobox 
+            options={cities} 
+            onChange={(value) => setCity(value)}
           />
 
           <DateRangePicker />
