b55ac291b35997fb94df7f9b3d3663adde472b13
bewuwy <61749397+bewuwy@users.noreply.github.com>
2024-05-22 19:49:33 +0000

    added dark mode

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,13 +2,13 @@
 export default function NavBar() {
 
     return (
-        <nav className="flex justify-between items-center p-4 text-black w-full absolute top-0">
+        <nav className="flex justify-between items-center p-4 w-full absolute top-0">
             <div className="flex items-center gap-4">
-                <a href="/" className="text-lg font-bold text-black">ROOM.nl archive</a>
-                <a href="/about" className="hover:underline text-black">About</a>
+                <a href="/" className="text-lg font-bold text-foreground">ROOM.nl archive</a>
+                <a href="/about" className="hover:underline text-foreground">About</a>
             </div>
             <div className="flex items-center gap-4">
-                <a href="https://github.com/bewuwy/roomnl-archive-web" target="blank" className="hover:underline text-black">Github</a>
+                <a href="https://github.com/bewuwy/roomnl-archive-web" target="blank" className="hover:underline text-foreground">Github</a>
             </div>
         </nav>
     )
