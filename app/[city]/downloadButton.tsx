e06451a1d79ffc96a7be1bd66ef300d7b46ce003
bewuwy <61749397+bewuwy@users.noreply.github.com>
2024-05-23 09:05:35 +0000

    added tooltip and icon to download button

--- app/[city]/downloadButton.tsx
+++ app/[city]/downloadButton.tsx
@@ -2,15 +2,29 @@
 
 import { Button } from "@/components/ui/button";
 import { to_download } from "@/lib/utils";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+
+import { Download } from 'lucide-react'
 
 export function DownloadButton({ data, filename }: { data: any[], filename: string }) {
-    return (
-        <Button onClick={() => {
-            to_download(data, filename, 
-                ["Address", "City", "Room type", "Number of reactions", "Contract date", "Account age (in days)", "Priority"]
-            );
-        }}>
-            Download
-        </Button>
-    );
+
+    const headers = ["Address", "City", "Room type", "Number of reactions", "Contract date", "Account age (in days)", "Priority"];
+
+    return (<TooltipProvider>
+    <Tooltip>
+        <TooltipTrigger asChild>
+            <Button onClick={() => {
+                to_download(data, filename, headers);
+            }}> 
+                Download <Download className="w-4 h-4 ml-2" />
+            </Button>
+        </TooltipTrigger>
+        <TooltipContent>Download data in the given timeframe in CSV format.</TooltipContent>
+    </Tooltip>
+    </TooltipProvider>)
 }
